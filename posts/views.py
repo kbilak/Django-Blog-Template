091b0169bd6bc231cbd210f4c0b12d51e85b7637
Krzysztof Białk <krzys.bialk1@gmail.com>
2022-04-05 14:47:51 +0200

    add search engine

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import *
 
 
@@ -56,9 +57,11 @@ def post_detail(request, id):
     # get post by id
     post = get_object_or_404(Post, id=id)
 
-    # get all comments assigned to post
-    comments = get_object_or_404(Comment, post=post.id)
+    # get all reviews of post
+    reviews = get_object_or_404(Review, post=post)
 
+    # get all comments assigned to post
+    comments = get_object_or_404(Comment, post=post)
 
     # get all comments replies
     all_replies = []
@@ -70,8 +73,39 @@ def post_detail(request, id):
     template = 'posts/post_detail.html'
     context = {
         'post': post,
+        'reviews': reviews,
         'comments': comments,
         'replies': all_replies,
+    }
+
+    return render(request, template, context)
+
+"""
+Search engine
+"""
+def search(request):
+    query = request.GET.get('q')
+
+    # filter published posts
+    search_result = Post.objects.filter(
+        status='2' and 
+        Q(translations__title__icontains=query) | 
+        Q(translations__body__icontains=query) | 
+        Q(category__icontains=query) | 
+        Q(author__icontains=query) |
+        Q(tags__icontains=query)
+    )
+    # set default ammount of results per page to 10
+    paginator = Paginator(search_result, 10)
+
+    # paginator config
+    page = request.GET.get('p')
+    venues = paginator.get_page(page)
+
+    # template path and context
+    template = 'posts/search_result.html'
+    context = {
+        'venues': venues,
     }
 
     return render(request, template, context)
